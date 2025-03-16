48d74edaebd01df3959e9e5658bd61cdc743ad39
NavidZamanKhan <navid166418@gmail.com>
2025-03-17 03:16:18 +0600

    added food categories ad grid view and added inkwell for tapping and effects

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    seedColor: const Color.fromARGB(255, 114, 30, 0),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
@@ -20,9 +21,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: theme,
-      home: Scaffold(appBar: AppBar(title: const Text("Hello World"))),
-    );
+    return MaterialApp(theme: theme, home: const CategoriesScreen());
   }
 }
